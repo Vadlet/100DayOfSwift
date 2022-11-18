5cbe03f107bafb38f2226ad31a6705fd4bad7cd3
Vadlet <letuchyvadim95@yandex.ru>
2022-11-18 13:33:22 +0300

    add 1 project

---
+++ 1/1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Vadlet on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
